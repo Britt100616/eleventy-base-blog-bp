00828761ba83517d0fae24f57db897918fc39691
Britt100616 <britneypease22@gmail.com>
2023-04-27 21:00:28 +0100

    added index.md

---
+++ .eleventy.config.js
@@ -0,0 +1,18 @@
+module.exports = function(eleventyConfig) {
+	return {
+		dir: {
+			_input: "content",
+			get input() {
+				return this._input;
+			},
+			set input(value) {
+				this._input = value;
+			},
+			output: "public"
+	
+		},
+	};
+
+;
+		}
+	
